56a1ab03e7ed72ddeeaca5743f256817db4d8a88
petersonstadler2 <petersonstadler@gmail.com>
2021-09-23 18:30:26 -0300

    Criado classe que sede herança para os objetos DAO. E criado select. Falta fazer o teste.

---
+++ DAO/DAO.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Gerenciador_de_Estoque.DAO
+{
+    class DAO
+    {
+        private readonly MySqlConnection conn;
+        private readonly MySqlCommand cmd;
+        string nometabela;
+        string[] NomeColunas;
+        string[] ApelidoColunas;
+
+        public DAO()
+        {
+            conn = new Conexao().GetConnection();
+            cmd = conn.CreateCommand();
+        }
+
+        public void CloseConnections()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Erro ao fechar conexão! \n\n" + e.Message, "Fechar Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string ColunasParaString(string[] NomeColunas)
+        {
+            string colunas = "";
+            for (int i = 0; i < NomeColunas.Length; i++)
+            {
+                if (i == 0)
+                {
+                    colunas += NomeColunas[i];
+                }
+                else
+                {
+                    colunas += ", " + NomeColunas[i];
+                }
+            }
+            return colunas;
+        }
+
+        private string ColunasParaString(string[] NomeColunas, string[] ApelidoColunas)
+        {
+            string colunas = "";
+            for(int i = 0; i < NomeColunas.Length; i++)
+            {
+                if(i == 0)
+                {
+                    colunas += NomeColunas[i] + " as " + ApelidoColunas[i];
+                }
+                else
+                {
+                    colunas += ", " + NomeColunas[i] + " as " + ApelidoColunas[i];
+                }
+            }
+            return colunas;
+        }
+
+        private string GerarSqlSELECT()
+        {
+
+            string sql = "SELECT " + ColunasParaString(NomeColunas, ApelidoColunas) + " FROM " + nometabela;
+            return sql;
+        }
+
+        public DataTable ListarEmDataTable()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = GerarSqlSELECT();
+            try
+            {
+                MySqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro ao listar Produtos! \n\n" + e.Message, "Listar Produtos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return dt;
+        }
+    }
+}
