874d2290fac9876e2cb8a221ff570ef5a30e572c
petersonstadler2 <petersonstadler@gmail.com>
2021-10-07 14:26:02 -0300

    Implementado sobrecarga em ItemNoPedidoDAO. Falta testar.

--- DAO/ItemNoPedidoDAO.cs
+++ DAO/ItemNoPedidoDAO.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gerenciador_de_Estoque.MODEL;
+using MySql.Data.MySqlClient;
 
 namespace Gerenciador_de_Estoque.DAO
 {
@@ -24,6 +26,43 @@ namespace Gerenciador_de_Estoque.DAO
 
             ColunasAlterar = ColunasInserir;
             ParametrosColunasAlterar = ParametrosColunasInserir;
+        }
+
+        protected override void AddParametrosInserir(object obj)
+        {
+            ItemNoPedido itemNoPedido = obj as ItemNoPedido;
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[0], itemNoPedido.Idpedido);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[1], itemNoPedido.Idproduto);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[2], itemNoPedido.Preco);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[3], itemNoPedido.Quantidade);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[4], itemNoPedido.Desconto);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[5], itemNoPedido.Acrescimo);
+        }
+
+        protected override void AddParametroAlterar(object obj)
+        {
+            ItemNoPedido itemNoPedido = obj as ItemNoPedido;
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[0], itemNoPedido.Idpedido);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[1], itemNoPedido.Idproduto);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[2], itemNoPedido.Preco);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[3], itemNoPedido.Quantidade);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[4], itemNoPedido.Desconto);
+            cmd.Parameters.AddWithValue(ParametrosColunasInserir[5], itemNoPedido.Acrescimo);
+        }
+
+        protected override object PreencherDados(MySqlDataReader dr)
+        {
+            ItemNoPedido itemNoPedido = new ItemNoPedido();
+            if (dr.Read())
+            {
+                itemNoPedido.Idpedido = dr.GetInt32(0);
+                itemNoPedido.Idproduto = dr.GetInt32(1);
+                itemNoPedido.Preco = dr.GetDecimal(2);
+                itemNoPedido.Quantidade = dr.GetInt32(3);
+                itemNoPedido.Desconto = dr.GetDecimal(4);
+                itemNoPedido.Acrescimo = dr.GetDecimal(5);
+            }
+            return itemNoPedido;
         }
     }
 }
