74c1ca3785964c932479ced6fe3485db4c148da6
petersonstadler2 <petersonstadler@gmail.com>
2021-11-04 09:22:26 -0300

    Adicionado FinanceiroDAO e Financeiro. Criado metodo CalcularFaturamento em FinanceiroDAO. Ainda não testado

---
+++ DAO/FinanceiroDAO.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace Gerenciador_de_Estoque.DAO
+{
+    class FinanceiroDAO : DAO
+    {
+        public decimal CalcularFaturamento(DateTime dataInicial, DateTime dataFinal)
+        {
+            decimal faturamento = 0;
+            string inicio = dataInicial.Year + "-" + dataInicial.Month + "-" + dataInicial.Day;
+            string final = dataFinal.Year + "-" + dataFinal.Month + "-" + dataFinal.Day;
+            try
+            {
+                cmd.CommandText = $"SELECT coalesce(SUM(preco), 0) as Faturamento  FROM pedidos_has_produtos WHERE (SELECT statuspedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Fechado' AND (SELECT financeiropedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Pago' AND (SELECT operacaopedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'SAIDA' AND (SELECT datapedido FROM pedidos WHERE idpedido = pedidos_idpedido) BETWEEN '{inicio}' AND '{final}';";
+                MySqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    faturamento = dr.GetDecimal("Faturamento");
+                }
+                dr.Close();
+            }catch(Exception e)
+            {
+                MessageBox.Show("Falha ao Calcular Faturamento! \n\n" + e, "Financeiro: Calcular Faturamento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return faturamento;
+        }
+    }
+}
---
+++ MODEL/Financeiro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_de_Estoque.MODEL
+{
+    class Financeiro
+    {
+        private DateTime dataInicial, dataFinal;
+        private decimal faturamento, lucro, fiado, custo;
+
+        public DateTime DataInicial { get => dataInicial; set => dataInicial = value; }
+        public DateTime DataFinal { get => dataFinal; set => dataFinal = value; }
+        public decimal Faturamento { get => faturamento; set => faturamento = value; }
+        public decimal Lucro { get => lucro; set => lucro = value; }
+        public decimal Fiado { get => fiado; set => fiado = value; }
+        public decimal Custo { get => custo; set => custo = value; }
+    }
+}
