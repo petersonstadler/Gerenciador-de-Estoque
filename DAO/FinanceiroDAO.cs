ebb5d9e0bfb8bb8f2a79a5c5866c78ec18ed9e51
petersonstadler2 <petersonstadler@gmail.com>
2021-11-04 11:43:28 -0300

    Criado CalcularGastosPorPeriodo, não testado.

--- DAO/FinanceiroDAO.cs
+++ DAO/FinanceiroDAO.cs
@@ -13,7 +13,7 @@ namespace Gerenciador_de_Estoque.DAO
             string final = dataFinal.Year + "-" + dataFinal.Month + "-" + dataFinal.Day + " 23-59-59";
             try
             {
-                cmd.CommandText = $"SELECT COALESCE(SUM(preco) - SUM(desconto) + SUM(acrescimo) - (SELECT SUM(fretepedido) FROM pedidos WHERE statuspedido = 'Fechado' AND operacaopedido = 'SAIDA'), 0) as Faturamento  FROM pedidos_has_produtos WHERE (SELECT statuspedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Fechado' AND (SELECT financeiropedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Pago' AND (SELECT operacaopedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'SAIDA' AND (SELECT datapedido FROM pedidos WHERE idpedido = pedidos_idpedido) BETWEEN '{inicio}' AND '{final}'";
+                cmd.CommandText = $"SELECT COALESCE(SUM(preco) - SUM(desconto) + SUM(acrescimo) - (SELECT SUM(fretepedido) FROM pedidos WHERE statuspedido = 'Fechado' AND operacaopedido = 'SAIDA'), 0) as Faturamento FROM pedidos_has_produtos WHERE (SELECT statuspedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Fechado' AND (SELECT financeiropedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Pago' AND (SELECT operacaopedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'SAIDA' AND (SELECT datapedido FROM pedidos WHERE idpedido = pedidos_idpedido) BETWEEN '{inicio}' AND '{final}'";
                 MySqlDataReader dr = cmd.ExecuteReader();
                 if (dr.Read())
                 {
@@ -25,6 +25,28 @@ namespace Gerenciador_de_Estoque.DAO
                 MessageBox.Show("Falha ao Calcular Faturamento! \n\n" + e, "Financeiro: Calcular Faturamento", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             return faturamento;
+        }
+
+        public decimal CalcularGastosPorPeriodo(DateTime dataInicial, DateTime dataFinal)
+        {
+            decimal gastos = 0;
+            string inicio = dataInicial.Year + "-" + dataInicial.Month + "-" + dataInicial.Day + " 00-00-00";
+            string final = dataFinal.Year + "-" + dataFinal.Month + "-" + dataFinal.Day + " 23-59-59";
+            try
+            {
+                cmd.CommandText = $"SELECT COALESCE(SUM(custo) - SUM(desconto) + SUM(acrescimo) + (SELECT SUM(fretepedido) FROM pedidos WHERE statuspedido = 'Fechado' AND operacaopedido = 'ENTRADA'), 0) as Gastos FROM pedidos_has_produtos WHERE (SELECT statuspedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Fechado' AND (SELECT financeiropedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'Pago' AND (SELECT operacaopedido FROM pedidos WHERE idpedido = pedidos_idpedido) = 'ENTRADA' AND (SELECT datapedido FROM pedidos WHERE idpedido = pedidos_idpedido) BETWEEN '{inicio}' AND '{final}'";
+                MySqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    gastos = dr.GetDecimal("Gastos");
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Falha ao Calcular Faturamento! \n\n" + e, "Financeiro: Calcular Faturamento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return gastos;
         }
     }
 }
--- MODEL/Financeiro.cs
+++ MODEL/Financeiro.cs
@@ -9,13 +9,13 @@ namespace Gerenciador_de_Estoque.MODEL
     class Financeiro
     {
         private DateTime dataInicial, dataFinal;
-        private decimal faturamento, lucro, fiado, custo;
+        private decimal faturamento, lucro, fiado, gastos;
 
         public DateTime DataInicial { get => dataInicial; set => dataInicial = value; }
         public DateTime DataFinal { get => dataFinal; set => dataFinal = value; }
         public decimal Faturamento { get => faturamento; set => faturamento = value; }
         public decimal Lucro { get => lucro; set => lucro = value; }
         public decimal Fiado { get => fiado; set => fiado = value; }
-        public decimal Custo { get => custo; set => custo = value; }
+        public decimal Gastos { get => gastos; set => gastos = value; }
     }
 }
